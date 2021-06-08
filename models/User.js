849db5b44c902ed6e158508265d36e9ef2342a87
zzkevin <realzzkevin@gmail.com>
2021-06-07 20:26:08 -0400

    index.js added

--- models/User.js
+++ models/User.js
@@ -9,7 +9,16 @@ class User extends Model {
 }
 
 User.init(
-  {
+  { 
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoincrement: true,
+    },
+    userName: {
+
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
