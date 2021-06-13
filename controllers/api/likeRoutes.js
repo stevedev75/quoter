8a279caae068bcdc6dd577e8fa0977e7c076a576
zzkevin <realzzkevin@gmail.com>
2021-06-13 15:03:46 -0400

    like routes done

---
+++ controllers/api/likeRoutes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const { json } = require('sequelize/types');
+const { Like } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res)=>{
+    try {
+        const newLike = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newLike);
+    }catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) =>{
+    try {
+        const like = await Like.destroy({
+            where:{
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if(!like) {
+            res.status(404).json({ message: `You didn't liked this quoter.`});
+        }
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
